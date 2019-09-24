5ab1aa611b8d805e6791eb9d2c9f659dc0c1e493
samrizzo <samrizzodev@gmail.com>
2019-09-24 00:17:11 -0400

    Added functionality to get data from the pokeapi.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import PokemonModel from './models/PokemonModel';
+import Pokemon from './components/Pokemon';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { pokemon: []};
+  }
+
+  componentDidMount() {
+    this.getPokemonData();
+  }
+
+  getPokemonData() {
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151')
+    .then(res => res.json())
+    .then((data) => {
+      let incrementId = 1;
+      let pokemonArray = data.results.map(function(item) {
+
+        let pokemonId;
+
+        if (incrementId < 10) 
+          pokemonId = `00${incrementId}`;
+        else if (incrementId >= 10 && incrementId < 100) 
+          pokemonId = `0${incrementId}`;
+        else 
+          pokemonId = incrementId;
+
+        let imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonId}.png`;
+        let pokemon = new PokemonModel(pokemonId, item.name, imageUrl);
+        incrementId++;
+
+        return pokemon;
+      });
+
+      this.updatePokemonState(pokemonArray);
+    })
+    .catch(console.log)
+  }
+
+  updatePokemonState(pokemonArray) {
+    this.setState({ pokemon: pokemonArray });
+  }
+
+  render() {
+    let pokeArray = this.state.pokemon;
+    pokeArray = pokeArray.filter(i => i.id <= 10);
+
+    return (
+      <div className="app">
+        <div className="poke-list">
+          { pokeArray.map(poke => <Pokemon pokemon={poke}/>) }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
